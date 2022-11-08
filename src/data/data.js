ece4eecf6c405457f0f77e05eaed8cf326419da5
Pilou-G <pierre.louis.goossens@gmail.com>
2022-11-08 16:25:29 +0100

    update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,4 @@
-export const apiURL = "https://api.belga.press/belgapress/api/organizations/674/quickintegrations/14ce205f-5963-478a-8bc9-bf874abfd0b6?count=50";
+export const apiURL = "";
 
 let offset = 50;
 
